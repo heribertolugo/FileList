816f83ac16dd428682046b5f6db7a62f132d78e4
Heriberto Lugo <heribertolugo_1@yahoo.com>
2019-10-24 01:36:08 -0400

    incrememnted version to reflect virtual scroll

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FileList")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Enumerates and lists all files in a directory")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Heriberto Lugo")]
 [assembly: AssemblyProduct("FileList")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
